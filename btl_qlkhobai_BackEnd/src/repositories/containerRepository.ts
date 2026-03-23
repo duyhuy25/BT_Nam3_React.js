bc1880856bc30cbb9ed118c3665fbd3e8edd62ee
duyhuy25 <duyhuy1342005@gmail.com>
2026-03-24 01:02:22 +0700

    update

--- btl_qlkhobai_BackEnd/src/repositories/containerRepository.ts
+++ btl_qlkhobai_BackEnd/src/repositories/containerRepository.ts
@@ -1,4 +1,5 @@
 import { poolPromise } from "../config/db";
+import sql from "mssql";
 
 export const getAllContainer = async () => {
   const pool = await poolPromise;
@@ -8,4 +9,51 @@ export const getAllContainer = async () => {
     .query("SELECT * FROM Container");
 
   return result.recordset;
+};
+
+export const createContainer = async (data: any) => {
+  const pool = await poolPromise;
+
+  await pool.request()
+    .input("HopDongID", sql.Int, data.HopDongID)
+    .input("LoaiHangID", sql.Int, data.LoaiHangID)
+    .input("TrongLuong", sql.Decimal(10, 2), data.TrongLuong)
+    .input("TrangThai", sql.NVarChar, data.TrangThai || "Rỗng")
+    .input("KhoID", sql.Int, data.KhoID)
+    .input("PhuongTienID", sql.Int, data.PhuongTienID)
+    .query(`
+      INSERT INTO Container (HopDongID, LoaiHangID, TrongLuong, TrangThai, KhoID, PhuongTienID)
+      VALUES (@HopDongID, @LoaiHangID, @TrongLuong, @TrangThai, @KhoID, @PhuongTienID)
+    `);
+};
+
+export const updateContainer = async (id: number, data: any) => {
+  const pool = await poolPromise;
+
+  await pool.request()
+    .input("ContainerID", sql.Int, id)
+    .input("HopDongID", sql.Int, data.HopDongID)
+    .input("LoaiHangID", sql.Int, data.LoaiHangID)
+    .input("TrongLuong", sql.Decimal(10, 2), data.TrongLuong)
+    .input("TrangThai", sql.NVarChar, data.TrangThai)
+    .input("KhoID", sql.Int, data.KhoID)
+    .input("PhuongTienID", sql.Int, data.PhuongTienID)
+    .query(`
+      UPDATE Container
+      SET HopDongID = @HopDongID,
+          LoaiHangID = @LoaiHangID,
+          TrongLuong = @TrongLuong,
+          TrangThai = @TrangThai,
+          KhoID = @KhoID,
+          PhuongTienID = @PhuongTienID
+      WHERE ContainerID = @ContainerID
+    `);
+};
+
+export const deleteContainer = async (id: number) => {
+  const pool = await poolPromise;
+
+  await pool.request()
+    .input("ContainerID", sql.Int, id)
+    .query("DELETE FROM Container WHERE ContainerID = @ContainerID");
 };
